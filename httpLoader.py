e4def4139077c06a3180c637ea75914c50494837
Dominik Schidlowski <dominik@schidlowski.eu>
2018-11-07 20:56:30 +0100

    add editor information to the http request to the droptable source

--- httpLoader.py
+++ httpLoader.py
@@ -1,18 +1,27 @@
 import urllib.request
+import urllib2
+
+def _loadWithoutCache(url):
+    req = urllib2.Request(url)
+    req.add_header('From', 'dominik@schidlowski.eu')
+    req.add_header('User-Agent', 'warframe-droptable-parser')
+    resp = urllib2.urlopen(req)
+    content = resp.read()
+    return content
 
 cache = {}
-def hasCache(name):
+def _hasCache(name):
     return name in cache
-def setCache(name, value):
+def _setCache(name, value):
     cache[name] = value
-def getCache(name):
+def _getCache(name):
     return cache[name]
 
 def load(url):
-    if hasCache(url):
-        return getCache(url)
+    if _hasCache(url):
+        return _getCache(url)
     print('miss ' + url)
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        setCache(url, html)
-        return html
+    html = _loadWithoutCache(url)
+    _setCache(url, html)
+    return html
+
