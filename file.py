d65c83b7848abb951ffe919b856c5846e484f96e
Dominik Schidlowski <dominik@schidlowski.eu>
2018-11-08 00:42:48 +0100

    add utf8 support

--- file.py
+++ file.py
@@ -4,5 +4,5 @@ def replace(fileName, content):
     if os.path.exists(fileName):
         os.remove(fileName)
 
-    with open(fileName, "w+") as f:
+    with open(fileName, "w+", "utf-8") as f:
         f.write(content);
