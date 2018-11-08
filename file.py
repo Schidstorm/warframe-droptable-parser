ea5a8da70eb87737d1ed62f77af24ca40ec9a08f
Dominik Schidlowski <dominik@schidlowski.eu>
2018-11-08 01:06:30 +0100

    utf8

--- file.py
+++ file.py
@@ -4,5 +4,5 @@ def replace(fileName, content):
     if os.path.exists(fileName):
         os.remove(fileName)
 
-    with open(fileName, "w+", "utf-8-sig") as f:
+    with open(fileName, "w+") as f:
         f.write(content);
